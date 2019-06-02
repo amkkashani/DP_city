1cf622873db776a0b5c57a790000384dd6ffb2d5
kash <amkkashani@gmail.com>
2019-06-02 18:19:05 +0430

    The End

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ class Main {
     public static int[][] monthCost,cityTravel,out;
     public static ArrayList<String> way;
     public static void main(String[] args) {
-//        way = new ArrayList<String>();
+        way = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
         n = Integer.parseInt(scanner.next());
         m = Integer.parseInt(scanner.next());
@@ -44,8 +44,6 @@ class Main {
                 out[j][i] = bestWay(i,j)+monthCost[j][i];
             }
         }
-
-
         for (int i=0;i<n;i++){
             System.out.print(cityNames.get(i)+" ");
             for (int j=0;j<m;j++){
@@ -53,6 +51,33 @@ class Main {
             }
             System.out.println();
         }
+
+        int min = -1;
+        int index=0;
+        for (int i=0;i<n;i++){
+            if(min == -1 || out[i][m-1]<min){
+                min = out[i][m-1];
+                index = i;
+            }
+        }
+        way.add(cityNames.get(index));
+        for (int i=m-2;i>=0;i--){
+            for (int j=0;j<n;j++){
+                if(out[j][i] < out[index][i+1]){
+                    if(out[j][i]+cityTravel[j][index]+monthCost[index][i+1] == out[index][i+1]){
+                        way.add(cityNames.get(j));
+                        index = j;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(min);
+        for (int i=m-1;i>=0;i--){
+            System.out.print(way.get(i)+" ");
+        }
+
+
     }
 
     public static int bestWay(int column,int row) {
