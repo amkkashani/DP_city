11fe47e07077c6135f207f0d5557d1b5e4e17dc0
kash <amkkashani@gmail.com>
2019-06-02 18:08:31 +0430

    finding best time by creating output matrix

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,69 @@
 import java.util.*;
 import javax.swing.tree.*;
 
-class Untitled {
+class Main {
 
-    public static  int n;
-    public static  int m;
-    public static  int n2;
-    public static  int m2;
-    public static  int[][] theArr;
-    public static  int[][] theArr2;
-
-    public static  int[][] theFinal;
-    public static ArrayList<String>names;
+    public static int n;//cities
+    public static int m;//months
+    public static ArrayList<String>cityNames;//city names
+    public static int[][] monthCost,cityTravel,out;
+    public static ArrayList<String> way;
     public static void main(String[] args) {
-        names = new ArrayList<String>();
-        theGetter();
-        theGetter2();
-        theMather();
-        test(n,m);
-
-    }
-
-    public static  void theGetter(){
-        Scanner s = new Scanner(System.in);
-        n = s.nextInt();
-        m = s.nextInt();
-        theArr = new int[n][m];
-        theFinal = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                theArr[i][j] = s.nextInt();
+//        way = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+        n = Integer.parseInt(scanner.next());
+        m = Integer.parseInt(scanner.next());
+        cityNames = new ArrayList<String>();
+        out = new int[n][m];
+        monthCost = new int[n][m];
+        cityTravel = new int[n][n];
+        //month stay in city costs input
+        System.out.println("month stay in city costs input");
+        for (int i=0;i<n;i++){
+            cityNames.add(scanner.next());
+            for (int j=0;j<m;j++){
+                monthCost[i][j] = Integer.parseInt(scanner.next());
             }
+        }
+        //city travel
+        System.out.println("city travel");
+        for (int i=0;i<n;i++){
+            scanner.next();
+            for (int j=0;j<n;j++){
+                cityTravel[i][j] = Integer.parseInt(scanner.next());
+            }
+        }
+
+
+        //calculating
+        for (int i=0;i<n;i++){
+            out[i][0] = monthCost[i][0];
+        }
+        for (int i=1;i<m;i++){
+            for (int j=0;j<n;j++){
+                out[j][i] = bestWay(i,j)+monthCost[j][i];
+            }
+        }
+
+
+        for (int i=0;i<n;i++){
+            System.out.print(cityNames.get(i)+" ");
+            for (int j=0;j<m;j++){
+                System.out.print(""+out[i][j]+" ");
+            }
+            System.out.println();
         }
     }
 
-    public static  void theGetter2(){
-        System.out.println("**");
-        Scanner s = new Scanner(System.in);
-        n2 = s.nextInt();
-        m2 = s.nextInt();
-        theArr2 = new int[n][m];
-        for (int i = 0; i < n2; i++) {
-            for (int j = 0; j < m2; j++) {
-                theArr2[i][j] = s.nextInt();
-            }
-        }
-    }
-
-    public static void theMather(){
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if(j==1){
-                    theFinal[i][j] = theArr[i][j];
-                }
-                else {
-                    theFinal[i][j] = theMinMiner(i, j);
-                }
-            }
-        }
-    }
-
-    public static int theMinMiner(int ii,int jj){
-        int min,key;
-        min = 100000;
-
-        for (int i = 0; i < n; i++) {
-            key = theFinal[i][jj-1] + theArr2[i][ii];
-            if (i == ii) {
-                key = theFinal[i][jj-1] + theArr[i][jj-1];
-            }
-            if (key<min) {
-                min = key;
+    public static int bestWay(int column,int row) {
+        int min =-1;
+        for (int i=0;i<n;i++){
+            int temp = out[i][column-1]+cityTravel[i][row];
+            if(temp < min || min == -1){
+                min = temp;
             }
         }
         return min;
-    }
-
-    public static void test(int n,int m){
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print("("+ theFinal[i][j]);
-            }
-            System.out.println("");
-
-        }
     }
 };
 
